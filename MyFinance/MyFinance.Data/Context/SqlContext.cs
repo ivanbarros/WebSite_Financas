3e3512cc541241ef515059aae316f77f4e2c5f68
@ivan.barros <ivan.barros@opah.com.br>
2022-01-02 23:45:00 -0300

    adding new class library and refactoring

---
+++ MyFinance/MyFinance.Data/Context/SqlContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFinance.Domain.Entities;
+
+namespace MyFinance.Data.Context
+{
+    public class SqlContextt : DbContext
+    {
+        public DbSet<ContaEntity> Conta { get; set; }
+        
+        public SqlContextt(DbContextOptions<SqlContextt> options) : base(options)
+{
+}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+
+}
