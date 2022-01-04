3019ff42f061c7b2bcade95f005ea39fe9de8239
ricardobauer <ricardo.bauer@vitalesolution.com.br>
2022-01-04 18:27:05 -0300

    create userentity

---
+++ MyFinance/MyFinance.Domain/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFinance.Domain.Entities
+{
+    public class UserEntity
+    {
+        public DateTime CreateDate { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
