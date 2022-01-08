bb22cedb8d3f1a0703fb49e0ea4ed6111bc1600d
ricardobauer <ricardo.bauer@vitalesolution.com.br>
2022-01-07 21:54:47 -0300

    Sales refactoring

--- MyFinance/MyFinance.Configurations/TableCreation/Sales.cs
+++ MyFinance/MyFinance.Configurations/TableCreation/Sales.cs
@@ -29,7 +29,7 @@ namespace MyFinance.Configurations.TableCreation
                 .NotNullable()
 
                 .WithColumn("DataCompra").AsDecimal()
-                .NotNullable()
+                .NotNullable();
         }
     }
 }
